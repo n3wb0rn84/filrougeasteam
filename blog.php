e55f202f3ae0a83bf2e1452cbcaee8e07090d6af
vanmaerckechri <vanmaerckechri@gmail.com>
2018-03-06 12:13:33 +0100

    categorie par and ou or pret

--- blog.php
+++ blog.php
@@ -6,11 +6,7 @@
     include 'humhum.php';
     //Importer le test de session.
     include 'session.php';
-//OPTIONS ADMIN!
-    if ($sessionOk)
-    {
-    }
-    //Connexion à la base de donnée.
+	//Connexion à la base de donnée.
     try
     {
       	// version Mysql
@@ -22,24 +18,19 @@
     {
         die('Erreur : ' . $e->getMessage());
     }
-    //Requête préparée.
-    $req = $bdd->prepare('SELECT * FROM articles ORDER BY id_articles DESC');
-    $req->execute();
-
-//FILTRES & PAGINATION!
-    //Récuperation des rangées de la table 'articles' dans un array.
-    $articlesArray = $req->fetchAll();
-    $articleNumberTotal = count($articlesArray);
-   	$articleNumberByPage = 5;
- 	//Système de tri par catégories.
- 	$articlesArrayFilter = array();
- 	//Variables qui serviront à garder les checkboxs séléctionnées lors du submit.
- 	$cat1 = '';
- 	$cat2 = '';
- 	$cat3 = '';
- 	$cat4 = '';
- 	//Tester si des categories ont été séléctionnées.
-
+//OPTIONS ADMIN!
+    if ($sessionOk)
+    {
+    }
+//TRIAGE
+    //Variables qui serviront à garder les checkboxs séléctionnées lors du submit.
+ 	$catCheck1 = '';
+ 	$catCheck2 = '';
+ 	$catCheck3 = '';
+ 	$catCheck4 = '';
+ 	$catbyand = '';
+ 	$catbyor = '';
+ 	//Récupérer les categories séléctionnées.
  	$categorie1 = htmlspecialchars(isset($_GET['categorie1'])) && htmlspecialchars($_GET['categorie1']) == true ? htmlspecialchars($_GET['categorie1']) : '';
 
 	$categorie2 = htmlspecialchars(isset($_GET['categorie2'])) && htmlspecialchars($_GET['categorie2']) == true ? htmlspecialchars($_GET['categorie2']) : '';
@@ -48,6 +39,10 @@
 
 	$categorie4 = htmlspecialchars(isset($_GET['categorie4'])) && htmlspecialchars($_GET['categorie4']) == true ? htmlspecialchars($_GET['categorie4']) : '';
 
+	$categorie5 = htmlspecialchars(isset($_GET['catbyand'])) && htmlspecialchars($_GET['catbyand']) == true ? htmlspecialchars($_GET['catbyand']) : '';
+
+	$categorie6 = htmlspecialchars(isset($_GET['catbyor'])) && htmlspecialchars($_GET['catbyor']) == true ? htmlspecialchars($_GET['catbyor']) : '';
+	//Tester s'il existe au moins une categorie séléctionnée.
  	if ($categorie1 != '' || $categorie2 != '' || $categorie3 != '' || $categorie4 != '')
  	{
  		$categories = array();
@@ -67,59 +62,92 @@
  		{ 		
 	 		array_push($categories, 4);
  		}
+ 		if ($catbyand == true)
+ 		{ 		
+	 		array_push($categories, 5);
+ 		}
+ 		if ($catbyor == true)
+ 		{ 		
+	 		array_push($categories, 6);
+ 		}
  		$categoriesLength = count($categories);
- 		//Pour garder les champs checked dans formulaire après submit. Cet entête de boucle sera executé une seconde fois, quelques lignes plus loin pour filtrer les articles. Cependant vu la nature de cette boucle, elle s'arrête lorsqu'elle trouve la premiere catégorie correspondant à l'article. Même si cet article se situe dans plusieurs de celles-ci. Elle ne pourra donc pas être utilisée pour valider les checkboxs.
+	 	//Garder les champs checked dans formulaire après submit.
 		for ($j = 0; $j < $categoriesLength; $j++)
 		{
 			switch ($categories[$j])
 		   	{
 		    case "1":
-		        $cat1 = 'checked';
+		        $catCheck1 = 'checked';
 		        break;
 		    case "2":
-		        $cat2 = 'checked';
+		        $catCheck2 = 'checked';
 		        break;
 		    case "3":
-		        $cat3 = 'checked';
+		        $catCheck3 = 'checked';
 		        break;
 		     case "4":
-		        $cat4 = 'checked';
+		        $catCheck4 = 'checked';
+		        break;
+		    case "5":
+		        $catbyand = 'checked';
+		        break;
+		     case "6":
+		        $catbyor = 'checked';
 		        break;
 			}
 		}
- 		//Boucle dans les articles...
-	 	for ($i = 0; $i < $articleNumberTotal; $i++)
-	 	{
-	 		//Boucle dans les catégories séléctionnées...
-	 		for ($j = 0; $j < $categoriesLength; $j++)
-	 		{
-	 			//Si l'article fait partie de la catégorie on l'ajoute dans l'array 'articlesArrayFilter' et on sort de la boucle 'catégories'. De plus, pour des raisons de sécurité on vérifie que l'élément de l'array 'catégories' est bien un nombre entier compris entre 1 et 4 (les 4 catégories).
-	 			if ($articlesArray[$i]['categorie'.$categories[$j].''] == true && $categories[$j] >= 1 && $categories[$j] <= 4)
-	 			{
-	 				array_push($articlesArrayFilter, $articlesArray[$i]);
-	 				break;
-	 			}
-	 		}
-	 	}
-	 	$articleNumberTotal = count($articlesArrayFilter);
-	 	//Si le nombre total d'article sur une page est inférieur au nombre total d'articles alors on fixe le nombre d'articles par page à ce dernier.
-	 	$articleNumberByPage = $articleNumberTotal < $articleNumberByPage ? $articleNumberTotal : $articleNumberByPage;
-	 	//S'il n'y aucun articles suite au tri, c'est qu'il y'a eu erreur ou injection => reinitialisation des données pour afficher la page sans catégories filtrées.
-		/*if ($articleNumberTotal == 0)
-		{
-			$articlesArrayFilter = $articlesArray;
-			$articleNumberTotal = count($articlesArrayFilter);
-		 	$articleNumberByPage = 5;
-		}*/
+		//Requête préparée.
+	    $req = $bdd->prepare('
+	    	SELECT * 
+	    	FROM articles
+	    	INNER JOIN relations
+	    	ON relations.id_articles = articles.id_articles
+	    	WHERE
+			 	relations.categorie1 = :cat1 AND relations.categorie1 = 1
+			OR
+				relations.categorie2 = :cat2 AND relations.categorie2 = 1
+			OR
+				relations.categorie3 = :cat3 AND relations.categorie3 = 1
+			OR
+				relations.categorie4 = :cat4 AND relations.categorie4 = 1
+	    	ORDER BY articles.id_articles DESC');
+	    $req->bindParam(':cat1', $categorie1, PDO::PARAM_BOOL);
+	   	$req->bindParam(':cat2', $categorie2, PDO::PARAM_BOOL);
+	   	$req->bindParam(':cat3', $categorie3, PDO::PARAM_BOOL);
+	    $req->bindParam(':cat4', $categorie4, PDO::PARAM_BOOL);
+	    $req->execute();
+	   	$articlesArray = $req->fetchAll();
+	   	$req->closeCursor();
+        $req = NULL;
+       	//Récuperation des rangées de la table 'articles' dans un array.
+	 	//$articlesArray = $req->fetchAll();
+	    $articleNumberTotal = count($articlesArray);
+	   	$articleNumberByPage = 5;
 	}
+	//Sinon, on charge tous les articles.
 	else
 	{
-		$articlesArrayFilter = $articlesArray;
+		$req = $bdd->prepare('
+	    SELECT * 
+	    FROM articles
+	   	ORDER BY id_articles DESC');
+	   	$req->execute();
+	   	$articlesArray = $req->fetchAll();
+	   	$req->closeCursor();
+        $req = NULL;
 	}
-
+ 	$articleNumberTotal = count($articlesArray);
+ 	//Si le nombre total d'article sur une page est inférieur au nombre total d'articles alors on fixe le nombre d'articles par page à ce dernier.
+ 	$articleNumberByPage = $articleNumberTotal < $articleNumberByPage ? $articleNumberTotal : $articleNumberByPage;
+ 	//S'il n'y aucun articles suite au tri, c'est qu'il y'a eu erreur ou injection => reinitialisation des données pour afficher la page sans catégories filtrées.
+	/*if ($articleNumberTotal == 0)
+	{
+		$articlesArrayFilter = $articlesArray;
+		$articleNumberTotal = count($articlesArrayFilter);
+	 	$articleNumberByPage = 5;
+	}*/
  	$pageNumberMax = $articleNumberTotal / $articleNumberByPage;
  	$pageNumberMax = ceil($pageNumberMax);
- 	$pageNumberRest = $pageNumberMax % $articleNumberByPage;
  	//Récuperer la valeur de la page transmise par l'url.
  	if (htmlspecialchars(isset($_GET['page'])))
  	{
@@ -143,7 +171,11 @@
  	//Changement d'index pour parcourir l'array 'articlesArray' en fonction de la page affichée.
  	$articlesOnThisPageFirstIndex = $articleNumberByPage * ($pageActuelle - 1);
  	//Changement du nombre d'articles à afficher si on se trouve sur la dernière page.
- 	$articleNumberByPage = $pageActuelle == $pageNumberMax ? ($articleNumberByPage - $pageNumberRest) : $articleNumberByPage;
+ 	/*$articleNumberByPage = $pageActuelle == $pageNumberMax ? ($articleNumberByPage - $pageNumberRest) : $articleNumberByPage;*/
+ 	if (!isset($pageNumberMax) || $pageNumberMax > 20)
+ 	{
+ 		$pageNumberMax = 1;
+ 	}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -160,13 +192,17 @@
 		<h1>Blog</h1>
 		<form action="blog.php" method="get">
 			<label for="categorie1">categorie1</label>
-			<input type="checkbox" name="categorie1" <?php echo $cat1;?>>
-			<label for="categorie2">categorie2</label>
-			<input type="checkbox" name="categorie2" <?php echo $cat2;?>>
-			<label for="categorie3">categorie3</label>
-			<input type="checkbox" name="categorie3" <?php echo $cat3;?>>
-			<label for="categorie4">categorie4</label>
-			<input type="checkbox" name="categorie4" <?php echo $cat4;?>>
+			<input type="checkbox" name="categorie1" <?php echo $catCheck1;?>>
+			<label for="categorie2"> | categorie2</label>
+			<input type="checkbox" name="categorie2" <?php echo $catCheck2;?>>
+			<label for="categorie3"> | categorie3</label>
+			<input type="checkbox" name="categorie3" <?php echo $catCheck3;?>>
+			<label for="categorie4"> | categorie4</label>
+			<input type="checkbox" name="categorie4" <?php echo $catCheck4;?>>
+			<label for="catbyand"> | AND</label>
+			<input type="radio" name="catandor" <?php echo $catbyand;?>>
+			<label for="catbyor"> | OR</label>
+			<input type="radio" name="catandor" <?php echo $catbyor;?>>
 			<input type="submit" value="valider">
 		</form>
 		<div class='articles'>
@@ -188,20 +224,16 @@
 		<?php
 			for ($i = 0, $j = $articlesOnThisPageFirstIndex; $i < $articleNumberByPage; $i++, $j++)
 	        {
-				if(isset($articlesArrayFilter[$j]))
+				if(isset($articlesArray[$j]))
 				{
 		?>
-					<h2><?php echo $articlesArrayFilter[$j]['titre']; ?><span> | <?php echo $articlesArrayFilter[$j]['date']; ?></span></h2>
-			       	<p><?php echo $articlesArrayFilter[$j]['contenu']; ?></p>
+					<h2><?php echo $articlesArray[$j]['titre']; ?><span> | <?php echo $articlesArray[$j]['date']; ?></span></h2>
+			       	<p><?php echo $articlesArray[$j]['contenu']; ?></p>
 		<?php
 		       }
 			}
 		?>
 		</div>
 	</div>
-	<?php
-	    $req->closeCursor();
-        $req = NULL;
-	?>
 </body>
 </html>
